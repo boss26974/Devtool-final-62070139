77cabb59ff5986f53364065aa6e202ed9211e542
boss26974 <pitchaya232@gmail.com>
2022-05-10 17:59:50 +0700

    finished

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -4,30 +4,24 @@
         <div class="container">
             <div class="row">
                 <div class="col-6 offset-3 align-self-center">
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Classmate Full Name</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="mate_fullname">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Classmate Studnt ID</label>
-                            <input type="text" class="form-control" id="exampleInputPassword1" v-model="mate_id">
-                        </div>
-                        <button class="btn btn-primary" @click="addClassMate">Submit</button>
-                </div>
-            </div>
-            <div class="row" style="margin-top: 60px">
-                <div class="col-3"  v-for="(mate,index) in all_Classmate" :key="mate.mate_id">
-                    <div class="card">
-                        <div class="card-header">
-                            My Classmate #{{index + 1}}
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Classmate Student ID</h5>
-                            <p class="card-text">{{mate.mate_id}}</p>
-                            <h5 class="card-title">Classmate Fullname</h5>
-                            <p class="card-text">{{mate.mate_fullname}}</p>
-                        </div>
+                    <div class="mb-3">
+                        <label for="disabledTextInput" class="form-label">ค้นหา 'คณะ'</label>
+                        <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
                     </div>
+                    <div class="mb-3">
+                        <label for="disabledTextInput" class="form-label">ค้นหา 'รอบรับสมัคร'</label>
+                        <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+                    </div>
+                    <div class="mb-3">
+                        <label for="disabledTextInput" class="form-label">ค้นหา 'โครงการ'</label>
+                        <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+                    </div>
+                    <div class="mb-3">
+                        <label for="disabledTextInput" class="form-label">ค้นหาหลักสูตร ไทย / นานาชาติ</label>
+                        <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+                    </div>
+                    <label for="inputPassword5" class="form-label">คำค้นหาเพิ่มเติม (Enter เพื่อแยกคำ) [ระบบจะค้นจากชื่อคณะ,หลักสูตร]</label>
+                    <input type="text" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
                 </div>
             </div>
         </div>
@@ -44,31 +38,9 @@ import axios from "axios"
                 all_Classmate: []
             }
         },
-        methods: {
-            addClassMate() {
-                if(this.mate_fullname == ""){
-                    alert("กรุณาใส่ชื่อนักศึกษาให้ถูกค้อง")
-                    return
-                }
-                if(this.mate_id == "" || this.mate_id.length != 8){
-                    alert("กรุณาใส่รหัสนักศึกษาให้ถูกค้อง")
-                    return
-                }
-                axios.post("http://localhost:3000/addstudents", {
-                    mate_id: this.mate_id,
-                    mate_fullname: this.mate_fullname
-                }).then((res) => {
-                    this.all_Classmate.push({
-                        mate_id: this.mate_id,
-                        mate_fullname: this.mate_fullname
-                    })
-                    console.log(res)
-                })
-            }
-        },
         created() {
             console.log("test")
-            axios.get("http://localhost:3000/allstudents")
+            axios.get("http://localhost:3000/allcourses")
             .then((res) => {
                 this.all_Classmate = res.data
             })
